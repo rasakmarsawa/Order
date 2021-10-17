bd30cdbc72db5ab0c5609232bd70f9bbe7a514c0
Yoga <rasakmarsawa@gmail.com>
2021-10-17 10:44:13 +0700

    Start

---
+++ controller/userController.php
@@ -0,0 +1,37 @@
+<?php
+include '../model/Kasir.php';
+
+/**
+ *
+ */
+class userController
+{
+
+  function __construct(argument){}
+
+  function getKasir(){
+
+  }
+
+  function addKasir(){
+
+  }
+
+  function deleteKasir(){
+
+  }
+
+  function getKasirById(){
+
+  }
+
+  function Login(){
+
+  }
+
+  function checkAdmin(){
+
+  }
+}
+
+?>
---
+++ model/Kasir.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ */
+class Kasir
+{
+  public $id_kasir;
+ 	public $nama_kasir;
+ 	public $username;
+  public $password;
+  public $admin;
+}
+
+?>
